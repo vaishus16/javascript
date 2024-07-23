699c9cf618e8547db3463ca09cf55981c56cf9be
vaishus16 <vaishu.erukala@gmail.com>
2024-07-23 13:09:08 +0530

    Initial commit

---
+++ DOM-1/script.js
@@ -0,0 +1,7 @@
+let a=10;    //assigning
+    a=20;  //reaasigning
+    function clickedme(){
+       document.getElementById("heading").innerText="this is modified heading";
+       document.getElementById("heading").style="color:red; font-size:60px";
+        alert("the heading got modified");
+    }
